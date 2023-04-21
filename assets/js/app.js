47c49a88bb505d39f65b8b924fcab025342854d1
l0bst3rr0r <davcoz31@gmail.com>
2023-04-21 18:14:07 +0100

    updated shit

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,23 @@ const observer = new IntersectionObserver((entries) => {
 
     });
 });
+let mybutton = document.getElementById("btnup");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  window.scrollTo({top: 0, behavior: 'smooth'});
+} 
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
